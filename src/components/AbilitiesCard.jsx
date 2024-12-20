076fdd0103b823a090604c262f170c9fddac0d79
adilhan <adilhansatymkulov40@gmail.com>
2024-12-21 00:47:38 +0600

    first commit

---
+++ src/components/AbilitiesCard.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+// import AbilityCardPhoto1 from './images/AbilitiesCardPhoto1.jpg'
+
+export const AbilitiesCard = () => {
+  return (
+    <div className='AbilitiesCard'> </div>
+  )
+}
