15e2d8c40c12596c042b79ede9ddad725d50e2fb
Ibro <bo1711357@gmail.com>
2024-12-21 23:56:05 +0600

    IbroTech

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import logo from './logo.svg';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link, useParams, NavLink } from "react-router-dom";
 import './App.css';
-import { Header } from './components/Header';
-import { Welcome } from './components/Welcome';
-import { Abilities } from './components/abilities';
+import { Home } from './components/Home';
+import logo from './components/images/logo.jpg';
 
-function App() {
+
+function About() {
+  return <h1>
+    This page will be made Ibro
+  </h1>;
+}
+function Projects() {
+  return <h1>
+    This page will be made Iliyar
+  </h1>;
+}
+
+function Navbar() {
   return (
-    <div className="App">
-      <Header/>
-      <Welcome/>
-      <Abilities/>
-    </div>
+    <nav className='header'>
+      <div className='LogoImage'>
+             <img src={logo} alt="" />
+        </div>
+        <div className='navSystem'>
+            <NavLink to="/"  className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
+            <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}> About</NavLink>
+            <NavLink to="/projects" className={({ isActive }) => (isActive ? "active" : "")}>Projects</NavLink>
+            <span className='Contact'>Contact us</span>
+        </div>
+    </nav>
   );
 }
 
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { Welcome } from './Welcome';
+import { Abilities } from './Abilities';
+
+export const Home = () => {
+    return ( <div>
+        <Welcome />
+        <Abilities/>
+    </div>
+        
+    )
+}
+export default Home;
