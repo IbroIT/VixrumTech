95f8c54bf3835ac4cc297fa8b6dd6f2cd4709779
adilhan <adilhansatymkulov40@gmail.com>
2024-12-21 23:11:14 +0600

    second commit adilhanpage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,37 @@ function App() {
       <Header/>
       <Welcome/>
       <Abilities/>
+
+      <div className='latestWorks w-[100vw] h-[100vh] flex items-center flex-col mt-[12vh]'>
+        <h1 className="w-[319px] h-[75px] font-nunito font-bold text-[55px] leading-[75px] tracking-[0.003em] text-[#377DFF] whitespace-nowrap">Latest Works</h1>
+        <p className='text-center w-[674px h-[67px] font-quicksand normal text-[#464646] font-semibold font-[20px] leading-[34px]'>all projects that we have already done , proven can help to use more <br></br> comfortable, then can used by client from our business</p>
+        
+
+
+      </div> 
     </div>
+
+    /* all projects that we have already done , proven can help to use more comfortable, then can used by client from our business */
+
+// position: absolute;
+// width: 674px;
+// height: 67px;
+// left: 282px;
+// top: 643px;
+
+// font-family: 'Quicksand';
+// font-style: normal;
+// font-weight: 400;
+// font-size: 20px;
+// line-height: 34px;
+// /* or 170% */
+// text-align: center;
+// letter-spacing: 0.01em;
+
+// /* grey color */
+// color: #464646;
+
+
   );
 }
 
