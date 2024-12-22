b707eb9aec25f1ee7115c81cb7580e5ac77fd99b
adilhan <adilhansatymkulov40@gmail.com>
2024-12-22 18:37:01 +0600

    commit adilhan

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { Header } from './components/Header';
 import { Welcome } from './components/Welcome';
 import { Abilities } from './components/abilities';
+import ProjectsShowing from './components/ProjectsShowing';
+import Offer from './components/Offer';
+import ContactForm from './components/ContactForm';
 
 function About() {
   return <h1>
@@ -32,9 +35,7 @@ function Navbar() {
     </nav>
   )}
   
-import ProjectsShowing from './components/ProjectsShowing';
-import Offer from './components/Offer';
-import ContactForm from './components/ContactForm';
+
 
 function App() {
   return (
