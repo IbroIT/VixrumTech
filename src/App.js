ef09a98cefb93ec742ec252d004c3f6cef4dc312
adilhan <adilhansatymkulov40@gmail.com>
2024-12-22 18:35:11 +0600

    Adilhan commit merge with IBro Routing

--- src/App.js
+++ src/App.js
@@ -50,16 +50,6 @@ function App() {
 
   );
 
-      <div className='latestWorks w-[100vw] h-[100vh] flex items-center flex-col mt-[12vh]'>
-        <h1 className="w-[319px] h-[75px] font-nunito font-bold text-[55px] leading-[75px] tracking-[0.003em] text-[#377DFF] whitespace-nowrap">Latest Works</h1>
-        <p className='text-center w-[674px h-[67px] font-quicksand normal text-[#464646] font-semibold font-[20px] leading-[34px]'>all projects that we have already done , proven can help to use more <br></br> comfortable, then can used by client from our business</p>
-        
-
-
-      </div> 
-      </div>
-
-  )
 }
 
 export default App;
