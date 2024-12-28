9bf302a2dbf40eba110fa98c8804896d9d85cb75
Ibro <ibroabdraimov20@gmail.com>
2024-12-28 19:18:56 +0600

    Ваше сообщение о коммите

--- src/App.js
+++ src/App.js
@@ -1,52 +1,12 @@
-import ReactDOM from "react-dom/client";
+import React from "react";
 import './App.css';
-import logo from './components/images/logo.jpg';
-import { Header } from './components/Header';
-import { About_1 } from './components/About_1';
-import { Welcome } from './components/Welcome';
-import { Abilities } from './components/abilities';
-
-function About() {
-  return <h1>
-    This page will be made Ibro
-  </h1>;
-}
-
-function Projects() {
-  return <h1>This page will be made Iliyar</h1>;
-}
-
-function Navbar() {
-  return (
-    <nav className='header'>
-      <div className='LogoImage'>
-        <img src={logo} alt="" />
-      </div>
-      <div className='navSystem'>
-        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
-        <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>About</NavLink>
-        <NavLink to="/projects" className={({ isActive }) => (isActive ? "active" : "")}>Projects</NavLink>
-        <span className='Contact'>Contact us</span>
-      </div>
-    </nav>
-
-  );
-}
-
+import './components/Header';
+import ReactDOM from 'react-dom/client';
 function App() {
   return (
-      <div className="App">
-      <Header/>
-      <Welcome/>
-      <Abilities/>
-
-      <div className='latestWorks w-[100vw] h-[100vh] flex items-center flex-col mt-[12vh]'>
-        <h1 className="w-[319px] h-[75px] font-nunito font-bold text-[55px] leading-[75px] tracking-[0.003em] text-[#377DFF] whitespace-nowrap">Latest Works</h1>
-        <p className='text-center w-[674px h-[67px] font-quicksand normal text-[#464646] font-semibold font-[20px] leading-[34px]'>all projects that we have already done , proven can help to use more <br></br> comfortable, then can used by client from our business</p>
-        
-
-
-      </div> </div> 
+    <div>
+      
+    </div>
   )
 }
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import logo from './images/logo.jpg'
 
 export const Header = () => {
   return (
-    <div className='header'>
-        <div className='LogoImage'>
-             <img src={logo} alt="" />
-        </div>
-        <div className='navSystem'>
-            <span className='home'>Home</span>
-            <span className='About'> About</span>
-            <span className='Projects'>Projects</span>
-            <span className='Contact'>Contact us</span>
-        </div>
+    <div className='Header' > 
+    
     </div>
   )
 }
